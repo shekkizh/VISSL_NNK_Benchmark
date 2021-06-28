55edd621171de62750ff3c3939398ad6b31edcb0
Sarath Shekkizhar <shekkizh@usc.edu>
2021-06-28 10:22:40 -0700

    adding utils files"

--- nnk_benchmark.py
+++ nnk_benchmark.py
@@ -14,6 +14,7 @@ from vissl.models.model_helpers import get_trunk_output_feature_names
 from vissl.utils.misc import merge_features
 from vissl.utils.checkpoint import get_checkpoint_folder
 from vissl.data.dataset_catalog import VisslDatasetCatalog
+from utils.non_neg_qpsolver import non_negative_qpsolver
 
 parser = argparse.ArgumentParser(description='VISSL extract features')
 parser.add_argument('--model_url',
@@ -25,8 +26,60 @@ parser.add_argument("--config", default="imagenet1k_resnet50_trunk_features.yaml
 parser.add_argument('--top_k', default=50, help="initial no. of neighbors")
 
 
-def nnk_classifier(features, labels, queries, targets, num_neighbors, num_classes=1000):
-    return 0, 0
+def to_categorical(y, num_classes=None, dtype='float32'):
+    """
+    Code taken from keras to categorical
+    """
+    y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes), dtype=dtype)
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
+
+
+@torch.no_grad()
+def nnk_classifier(features, labels, queries, targets, topk, num_classes=1000):
+    dim = features.size(1)
+    target_one_hot = to_categorical(labels, num_classes)
+    normalized_features = features / np.linalg.norm(features, axis=1, keepdims=True)
+    index = faiss.IndexFlatIP(dim)
+    index = faiss.index_cpu_to_all_gpus(index)
+    index.add(normalized_features)
+
+    normalized_queries = queries / np.linalg.norm(queries, axis=1, keepdims=True)
+    n_queries = queries.shape[0]
+    soft_prediction = np.zeros(shape=(n_queries, num_classes), dtype=np.float)
+
+    distances, indices = index.search(normalized_queries, topk)
+
+    for ii, x_test in enumerate(normalized_queries):
+        neighbor_indices = indices[ii, :]
+        neighbor_labels = target_one_hot[neighbor_indices, :]
+        x_support = normalized_features[neighbor_indices]
+        g_i = 0.5 + np.dot(x_support, x_test) / 2
+        G_i = 0.5 + np.dot(x_support, x_support.T) / 2
+        x_opt = non_negative_qpsolver(G_i, g_i, g_i, x_tol=1e-10)
+        non_zero_indices = np.nonzero(x_opt)
+        x_opt = x_opt[non_zero_indices] / np.sum(x_opt[non_zero_indices])
+        soft_prediction[ii, :] = np.dot(x_opt, neighbor_labels[non_zero_indices])
+        if ii % 10000 == 0:
+            print(f"{ii}/{n_queries} processed...")
+
+    probs = torch.from_numpy(soft_prediction).cuda()
+    targets = targets.cuda()
+    _, predictions = probs.sort(1, True)
+    correct = predictions.eq(targets.data.view(-1, 1))
+    top1 = correct.narrow(1, 0, 1).sum().item() * 100.0 / n_queries
+    top5 = correct.narrow(1, 0, 5).sum().item() * 100.0 / n_queries
+    return top1, top5
 
 
 def benchmark_layer(cfg: AttrDict, layer_name: str = "heads"):
@@ -45,18 +98,19 @@ def benchmark_layer(cfg: AttrDict, layer_name: str = "heads"):
     return top1, top5
 
 
-def hydra_main(overrides: List[Any]):
+def hydra_main(overrides: List[Any], extract_features=False):
     print(f"####### overrides: {overrides}")
     with initialize_config_module(config_module="vissl.config"):
         cfg = compose("defaults", overrides=overrides)
     args, config = convert_to_attrdict(cfg)
-    # set_env_vars(local_rank=0, node_id=0, cfg=config)
-    launch_distributed(
-        cfg=config,
-        node_id=args.node_id,
-        engine_name=args.engine_name,
-        hook_generator=default_hook_generator,
-    )
+
+    if extract_features:
+        launch_distributed(
+            cfg=config,
+            node_id=args.node_id,
+            engine_name=args.engine_name,
+            hook_generator=default_hook_generator,
+        )
 
     feat_names = get_trunk_output_feature_names(config.MODEL)
     if len(feat_names) == 0:
@@ -64,6 +118,7 @@ def hydra_main(overrides: List[Any]):
 
     for layer in feat_names:
         top1, top5 = benchmark_layer(config, layer_name=layer)
+        print(f"NNK classifier - Layer: {layer}, Top1: {top1}, Top5: {top5}")
 
 
 if __name__ == "__main__":
---
+++ utils/non_neg_qpsolver.py
@@ -0,0 +1,76 @@
+__author__ = "shekkizh"
+"""Custom solver for solving non negative quadratic programs with positive definite matrices"""
+
+import numpy as np
+from scipy.linalg import get_lapack_funcs
+import warnings
+
+
+def non_negative_qpsolver(A, b, x_init, x_tol, check_tol=-1, epsilon_low=-1, epsilon_high=-1):
+    """
+    Solves (1/2)x.T A x - b.T x
+    :param x_init: Initial value for solution x
+    :param x_tol: Smallest allowed non zero value for x_opt. Values below x_tol are made zero
+    :param check_tol: Allowed tolerance for stopping criteria. If negative, uses x_tol value
+    :param epsilon_high: maximum value of x during optimization
+    :param epsilon_low: minimum value of x during optimization
+    :return: x_opt, error
+    """
+    if epsilon_low < 0:
+        epsilon_low = x_tol  # np.finfo(float).eps
+    if epsilon_high < 0:
+        epsilon_high = x_tol
+    if check_tol < 0:
+        check_tol = x_tol
+
+    n = A.shape[0]
+    # A = A + 1e-6 * np.eye(n)
+    max_iter = 50 * n
+    itr = 0
+    # %%
+    x_opt = np.reshape(x_init, (n, 1))
+    N = 1.0 * (x_opt > (1 - epsilon_high))  # Similarity too close to 1 (nodes collapse)
+    if np.sum(N) > 0:
+        x_opt = x_opt * N
+        return x_opt[:, 0]
+
+    # %%
+    non_pruned_elements = x_opt > epsilon_low
+    check = 1
+
+    while (check > check_tol) and (itr < max_iter):
+        x_opt_solver = np.zeros((n, 1))
+        x_opt_solver[non_pruned_elements] = cholesky_solver(
+            A[non_pruned_elements[:, 0], :][:, non_pruned_elements[:, 0]], b[non_pruned_elements[:, 0]], tol=x_tol)
+        x_opt = x_opt_solver
+        itr = itr + 1
+        N = x_opt < epsilon_low
+        if np.sum(N) > 0:
+            check = np.max(np.abs(x_opt[N]))
+        else:
+            check = 0
+        non_pruned_elements = np.logical_and(x_opt > epsilon_low, non_pruned_elements)
+
+    x_opt[x_opt < x_tol] = 0
+    return x_opt[:, 0]
+
+def cholesky_solver(a, b, tol=1e-10, lower=False, overwrite_a=False, overwrite_b=False, clean=True):
+    """Modified code from SciPy LinAlg routine"""
+
+    a1 = np.atleast_2d(a)
+    # Quick return for square empty array
+    if a1.size == 0:
+        return b
+
+    potrf, = get_lapack_funcs(('potrf',), (a1,))
+    c, info = potrf(a1, lower=lower, overwrite_a=overwrite_a, clean=clean)
+
+    if info > 0:
+        warnings.warn("Cholesky solver encountered positive semi-definite matrix -- possible duplicates in data")
+        # return solve(a1, b, assume_a='sym', lower=lower, overwrite_a=overwrite_a, overwrite_b=overwrite_b,
+        #              check_finite=False)
+        c = c + tol*np.eye(b.size)
+
+    potrs, = get_lapack_funcs(('potrs',), (c, b))
+    x, info = potrs(c, b, lower=lower, overwrite_b=overwrite_b)
+    return x
